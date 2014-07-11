8f92b3879ff19dd6c35d703fbc394771f6ad1d9b
deep-orca <deep.orca@gmail.com>
2014-07-11 11:43:28 +0400

    cosmetics

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ Flask-MetaRoute
 -------------
 
 Flask-MetaRoute adds some useful decorators for routing
+Sample app available at https://github.com/deep-orca/Flask-MetaRoute-sample-app
 """
 from setuptools import setup
 
